6791f6ddab891e30845f6fb08af02fdeedd7b780
Raiane Caroline S Teixeira <raianne.caroline@redeinova.net>
2023-10-06 03:45:21 -0300

    Página de procurar por letras feita, falta corrigir bug de receitas e estilizar páginas

--- src/components/RecipesIngredients/index.tsx
+++ src/components/RecipesIngredients/index.tsx
@@ -1,10 +1,53 @@
-import Header from "../Header"
+import { useState, useEffect } from "react";
+import SearchFoodComponent from "../SearchFood";
+
+type Ingredient = {
+  strMeal: string,
+  idMeal: string,
+}
 
 const RecipesIngredientsComponent: React.FC = () => {
+  
+  const [data, setData] = useState<Ingredient[]>([])
+
+  const getIngredients = async (name: string) => {
+    try {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${name}`);
+      const data = await response.json();
+      
+      if (data.ingredients && data.ingredients.length > 0) {
+        setData(data.ingredients);
+      } else {
+        setData([]);
+      }
+    } catch (error) {
+      console.error("Erro ao buscar ingredientes:", error);
+    }
+  }
+
+  useEffect(() => {
+    getIngredients('');
+  }, [])
+
+  console.log({data})
+
   return (
     <>
-      <Header/>
-      <h1>Receitas Por Ingredientes</h1>
+      <div>
+        <SearchFoodComponent label="Ingredientes" onSearch={getIngredients}/>
+
+        <br />
+        {data.map((ingredientes) => {
+          const { strMeal, idMeal } = ingredientes
+
+          return (
+            <div key={idMeal}>
+              <a href=""></a>
+              <h3>{strMeal}</h3>
+            </div>
+          )
+        })}
+      </div>
     </>
   )
 }
--- src/components/SearchFoodLetters/index.tsx
+++ src/components/SearchFoodLetters/index.tsx
@@ -1,15 +1,82 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { ReactNode } from "react"
-import Header from "../Header"
+import React, { useState, useEffect } from "react";
+import Header from "../Header";
+
+interface Meal {
+  idMeal: string;
+  strMeal: string;
+  strMealThumb: string;
+  strInstructions: string;
+  strYoutube: string;
+}
 
 const SearchFoodLettersComponent: React.FC = () => {
-  return (
+  const [data, setData] = useState<Meal[]>([]);
+  const [selectedLetter, setSelectedLetter] = useState<string>('');
 
+  const arrayOfLetters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+  const getMealByLetter = async (letter: string) => {
+    try {
+      const mealResponse = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`);
+      const mealData = await mealResponse.json();
+
+      console.log(mealData);
+
+      const { meals } = mealData;
+      if (meals) {
+        setData(meals);
+      }
+    } catch (error) {
+      console.error("Erro ao buscar refeições:", error);
+    }
+  }
+
+  const handleOnClickButton = (letter: string) => {
+    return () => {
+      setSelectedLetter(letter);
+      getMealByLetter(letter);
+    }
+  }
+
+  useEffect(() => {
+    if (selectedLetter === '') {
+      getMealByLetter('a');
+    }
+  }, [selectedLetter]);
+
+  return (
     <>
-      <Header/>
-      <h1>Food Letters</h1>
+      <Header />
+      <div className="container-buttons">
+        {arrayOfLetters.map((letter) => (
+          <button
+            key={letter}
+            onClick={handleOnClickButton(letter)}
+            className={letter === selectedLetter ? "selected" : ""}
+          >
+            {letter}
+          </button>
+        ))}
+      </div>
+      {data.length > 0 && (
+        <div className="meal-list">
+          <h2>Refeições que começam com a letra '{selectedLetter}'</h2>
+          <ul>
+            {data.map((meal) => (
+              <li key={meal.idMeal}>
+                <img src={meal.strMealThumb} alt={meal.strMeal} />
+                <h2>{meal.strMeal}</h2>
+                <p>{meal.strInstructions}</p>
+                <button><a href={meal.strYoutube} target="_blank" rel="noopener noreferrer">Ver Detalhes</a></button>
+                
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </>
   )
 }
 
-export default SearchFoodLettersComponent
+export default SearchFoodLettersComponent;
+
